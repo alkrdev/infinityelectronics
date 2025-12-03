4453cda58a0e3e5037f458ddf72edb670a9c42d8
Alexander <alkrdev@gmail.com>
2025-12-03 10:09:24 +0100

    initial commit

---
+++ app/products/page.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import Image from "next/image";
+
+interface Product   {
+  id: number,
+  title: string,
+  price: number,
+  description: string,
+  category: string,
+  image: string,
+  rating: Rating
+}
+
+interface Rating {
+  rate: number,
+  count: number
+}
+
+export default async function Home() {
+  const data = await fetch('https://fakestoreapi.com/products')
+  const products = await data.json()
+
+
+  return (
+    <div className="w-4/6 mx-auto">
+      <nav className="p-4 border-b flex justify-between items-center">
+        <h1 className="text-2xl font-bold">InfinityElectronics</h1>
+        <div className="flex flex-row gap-4">
+          <Link href="/products">Products</Link>
+          <Link href="/products">Page</Link>
+          <Link href="/products">Page</Link>
+          <Link href="/products">Page</Link>
+        </div>
+      </nav>
+      <main className="p-4">
+        <h2 className="text-xl mb-4">Welcome to infinite opportunities</h2>
+        <div className="bg-amber-200 text-black h-96">Slider</div>
+        <div className="grid grid-cols-5 gap-4 mt-4">
+          {products.map((product: Product) => (
+            <div key={product.id}>
+              <Image src={product.image} alt="productimage" width="200" height="200"></Image>
+              <div>Product Name</div>
+              <div>Category</div>
+            </div>
+          ))}
+          
+        </div>
+      </main>
+    </div>
+  );
+}
