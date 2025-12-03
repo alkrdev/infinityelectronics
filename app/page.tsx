d968af320f1e43bf95a59d4c0b4ac625b7ce382e
Alexander <alkrdev@gmail.com>
2025-12-03 12:31:58 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,7 @@ export default function Home() {
 
   return (
     <div className="w-4/6 mx-auto">
-      <Navbar />
-      <main className="p-4">
-        <h2 className="mb-4">Welcome to infinite opportunities</h2>
-        <div className="bg-green-700 text-black h-96">Slider</div>
-        <Suspense fallback={<div>Loading...</div>}>          
-          <Products products={productsPromise} />
-        </Suspense>
-      </main>
+      
     </div>
   );
 }
