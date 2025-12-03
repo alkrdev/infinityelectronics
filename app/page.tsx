acc2138a16e847b51582d5c2d827953985c711d6
Alexander <alkrdev@gmail.com>
2025-12-03 12:33:25 +0100

    test2

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Navbar from "./_components/navbar";
 import { getProducts } from "./_lib/utils";
 
 export default function Home() { 
-  const productsPromise = getProducts();
 
   return (
     <div className="w-4/6 mx-auto">
