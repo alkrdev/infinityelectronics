8b70f5b10965f710d2e72e8b14f74ab79f5fb635
Alexander <alkrdev@gmail.com>
2025-12-03 12:30:58 +0100

    styling adjustments

--- app/_components/products.tsx
+++ app/_components/products.tsx
@@ -1,28 +1,27 @@
-'use client'
-import { Product } from "@/_interfaces/product.interface";
-import Image from "next/image";
-import Link from "next/link";
-import { use } from "react";
+'use client';
+import { Product } from '@/_interfaces/product.interface';
+import Image from 'next/image';
+import Link from 'next/link';
+import { use } from 'react';
 
-export default function Products({
-  products,
-}: {
-  products: Promise<Product[]>
-}) {
-  const allProducts = use(products)
-            
- 
-  return (
-    <div className="grid grid-cols-5 gap-4 mt-4">
-        {allProducts.map((product: Product) => (
-            <Link href={"/products/" + product.id} key={product.id} className="p-4 hover:bg-emerald-700 hover:cursor-pointer flex flex-col justify-center">
-                <div className="relative mb-4 size-[225px] mx-auto">
-                    <Image src={product.image} alt="productimage" fill={true}></Image>
-                </div>
-                <div>{product.title}</div>
-                <div>{product.category}</div>
-            </Link>
-        ))}
-    </div>
-  )
+export default function Products({ products }: { products: Promise<Product[]> }) {
+	const allProducts = use(products);
+
+	return (
+		<div className='grid grid-cols-5 gap-4 mt-4'>
+			{allProducts.map((product: Product) => (
+				<Link
+					href={'/products/' + product.id}
+					key={product.id}
+					className='p-4 bg-gray-600 hover:bg-emerald-700 hover:cursor-pointer flex flex-col justify-center'
+				>
+					<div className='relative mb-4 size-[200px] mx-auto'>
+						<Image src={product.image} alt='productimage' fill={true} objectFit='contain'></Image>
+					</div>
+					<div className='text-xl'>{product.title}</div>
+					<div>{product.category}</div>
+				</Link>
+			))}
+		</div>
+	);
 }
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from 'react';
 import Image from 'next/image';
 import Navbar from '@/app/_components/navbar';
-
 import { getProductById } from '@/app/_lib/utils';
 
 export default async function Product({ params }: { params: { id: number } }) {
@@ -14,10 +13,10 @@ export default async function Product({ params }: { params: { id: number } }) {
 			<main className='p-4'>
 				<Suspense fallback={<div>Loading...</div>}>
 					<div className='flex flex-row items-center justify-center gap-16'>
-						<div className="relative w-[350px] h-[500px]">
-							<Image fill src={product.image} alt='product'></Image>
+						<div className="relative size-[700px] m-16">
+							<Image fill src={product.image} objectFit='contain' alt='product'></Image>
 						</div>
-						<div className="flex flex-col gap-4 w-[450px]">
+						<div className="flex flex-col gap-4 w-[450px] m-16">
 							<h2>{product.title}</h2>
 							<p>{product.description}</p>
 							<div className="bg-green-700 w-[200px] h-[60px] flex items-center justify-center text-2xl">Add to cart</div>
