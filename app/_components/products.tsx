22c0b5a8954238eceed44763d7aa207576136bf8
Alexander <alkrdev@gmail.com>
2025-12-03 12:00:02 +0100

    Product navigation

--- app/_components/products.tsx
+++ app/_components/products.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Product } from "@/_interfaces/product.interface";
 import Image from "next/image";
+import Link from "next/link";
 import { use } from "react";
 
 export default function Products({
@@ -14,11 +15,13 @@ export default function Products({
   return (
     <div className="grid grid-cols-5 gap-4 mt-4">
         {allProducts.map((product: Product) => (
-            <div key={product.id}>
-                <Image src={product.image} alt="productimage" width="200" height="200"></Image>
-                <div>Product Name</div>
-                <div>Category</div>
-            </div>
+            <Link href={"/products/" + product.id} key={product.id} className="p-4 hover:bg-emerald-800 hover:cursor-pointer flex flex-col justify-center">
+                <div className="relative mb-4 size-[225px] mx-auto">
+                    <Image src={product.image} alt="productimage" fill={true}></Image>
+                </div>
+                <div>{product.title}</div>
+                <div>{product.category}</div>
+            </Link>
         ))}
     </div>
   )
