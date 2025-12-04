b6d9fa56b382c681d7f7bc8d5c23bddbadef0437
Alexander <alkrdev@gmail.com>
2025-12-04 17:01:26 +0100

    don't suspend, make it a client

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,4 +1,5 @@
-import { Suspense, useEffect, useState } from 'react';
+'use client'
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Navbar from '@/app/components/navbar';
 import { Product } from '@/app/interfaces/product.interface';
@@ -38,18 +39,16 @@ export default function ProductPage({ params }: { params: { id: number } }) {
 		<div className='w-4/6 mx-auto'>
 			<Navbar />
 			<main className='p-4'>
-				<Suspense fallback={<div>Loading...</div>}>
-					<div className='flex flex-row items-center justify-center gap-16'>
-						<div className="relative size-[700px] m-16">
-							<Image fill src={product.image} objectFit='contain' alt='product'></Image>
-						</div>
-						<div className="flex flex-col gap-4 w-[450px] m-16">
-							<h2>{product.title}</h2>
-							<p>{product.description}</p>
-							<div className="bg-green-700 w-[200px] h-[60px] flex items-center justify-center text-2xl">Add to cart</div>
-						</div>
+				<div className='flex flex-row items-center justify-center gap-16'>
+					<div className="relative size-[700px] m-16">
+						<Image fill src={product.image} objectFit='contain' alt='product'></Image>
 					</div>
-				</Suspense>
+					<div className="flex flex-col gap-4 w-[450px] m-16">
+						<h2>{product.title}</h2>
+						<p>{product.description}</p>
+						<div className="bg-green-700 w-[200px] h-[60px] flex items-center justify-center text-2xl">Add to cart</div>
+					</div>
+				</div>
 			</main>
 		</div>
 	);
