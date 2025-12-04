594aa5a226eec0fff92900ad3294d5bcd423d96a
Alexander <alkrdev@gmail.com>
2025-12-04 16:59:15 +0100

    I said everything

--- app/api/products/route.ts
+++
@@ -1,8 +0,0 @@
-
-export async function GET() {
-    const url = process.env.API_URL || 'https://fakestoreapi.com';
-
-    const res = await fetch(url + '/products');
-    const data = await res.json();
-    return Response.json(data);
-}
