aae90cafe9e9e5813fb79484274b71a0226f8e6c
Alexander <alkrdev@gmail.com>
2025-12-04 16:19:43 +0100

    incorrect name

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,11 +1,11 @@
 export async function GET(
   request: Request,
-  { params }: { params: Promise<{ slug: string }> }
+  { params }: { params: Promise<{ id: string }> }
 ) {
     const url = process.env.API_URL || 'https://fakestoreapi.com';
-    const { slug } = await params;
+    const { id } = await params;
 
-    const res = await fetch(url + '/products/' + slug);
+    const res = await fetch(url + '/products/' + id);
     const data = await res.json();
     return Response.json(data);
 }
